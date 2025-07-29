ed207bc24843bb504b07863e4537778ae34c00f6
nozima-abdurahmanova <nozima.abdurakhmanova088@gmail.com>
2025-07-29 20:33:56 +0500

    lesson 9
    lesson 9

---
+++ lesson999.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 20.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{35BD599A-2ED4-4C9E-B58A-FD1AD7A00333}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {EBFD9D44-EAC3-41C9-B7DA-9DD138D2A26F}
+	EndGlobalSection
+EndGlobal
